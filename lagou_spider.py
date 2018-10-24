3b88cf31d8e40ec96fc00c4dda7b546ab2cb3c2b
jack161616 <strong0528@aliyun.com>
2018-10-24 15:05:56 +0800

    second commit

--- lagou_spider.py
+++ lagou_spider.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pylab as plt
 import jieba
 from wordcloud import WordCloud
+import math
 
 # 该爬虫爬取拉勾网用户想要查询的地区的python相关招聘信息,并且进行数据处理与分析.
 
@@ -22,7 +23,7 @@ def get_json(kind, city, page=1,):
         'kd':kind
     }
     # 想要搜索的城市名称
-    kw = {'city':city,'needAddtionalResult':'false'}
+    kw = {'px':'default', 'city':city, 'needAddtionalResult':'false'}
 
     user_agent_list = ['Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36',
                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
@@ -79,12 +80,17 @@ if __name__ == '__main__':
     # 招聘总条数
     total = response_result['totalCount']
     print('{}开发职位,招聘信息总共{}条.....'.format(kind, total))
-
+    #每页15条 向上取整 算出总页数 , 爬取的页码不超过30页
+    page_total = math.ceil(total / 15)
+    print('page_total:',page_total)
+    if page_total > 30:
+        page_total = 30
+    print('page_total_finall:', page_total)
     # 所有查询结果
     search_job_result = []
 
-    # 拉钩网爬取30页面信息
-    for i in range(1, 30):
+    # 拉钩网爬取页面信息
+    for i in range(1, page_total+1):
         response_result = get_json(kind, city, page=i)
         # # 每次抓取完成后,暂停一会,防止被服务器拉黑
         time.sleep(15)
@@ -124,12 +130,12 @@ if __name__ == '__main__':
 
         df = pd.DataFrame(data=search_job_result,
                           columns=['公司全名', '公司简称', '公司规模', '融资阶段', '区域', '职位名称', '工作经验', '学历要求', '工资', '职位福利'])
-        df.to_csv('lagou.csv', index=False, encoding='utf-8_sig')
+        df.to_csv(city+'_'+kind+'_'+'lagou.csv', index=False, encoding='utf-8_sig')
 
     # ---------------数据分析--------------
     #剔除实习岗位的招聘、工作年限无要求或者应届生的当做0年处理、薪资范围需要计算出一个大概的值、学历无要求的当成大专
     # 读取数据
-    df = pd.read_csv('lagou.csv', encoding='utf-8')
+    df = pd.read_csv(city+'_'+kind+'_'+'lagou.csv', encoding='utf-8')
     # 数据清洗,剔除实习岗位
     df.drop(df[df['职位名称'].str.contains('实习')].index,inplace=True)
     # 由于CSV文件内的数据是字符串形式,先用正则表达式将字符串转化为列表,再取区间的均值
@@ -162,38 +168,39 @@ if __name__ == '__main__':
     avg_salary = []
     for k in df['salary']:
         int_list = [int(n) for n in k]
-        avg_wage = int_list[0] + (int_list[1] - int_list[0]) / 4
+        # 存在工资只有一个区间,[20],所以用[-1].
+        avg_wage = int_list[0] + (int_list[-1] - int_list[0]) / 4
         avg_salary.append(avg_wage)
     df['月工资'] = avg_salary
 
     # 将学历不限的职位要求认定为最低学历:大专\
     df['学历要求'] = df['学历要求'].replace('不限', '大专')
 
-    df.to_csv('lagou_finall.csv', index=False, encoding='utf-8_sig')
+    df.to_csv(city+'_'+kind+'_'+'lagou_finall.csv', index=False, encoding='utf-8_sig')
 
     #-------------数据图形可视化--------------
 
     # 中文乱码的处理
-    plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-    plt.rcParams['axes.unicode_minus'] = False
+    plt.rcParams['font.sans-serif'] = ['simhei']
+    plt.rcParams['font.serif'] = ['simhei']
 
     #绘制薪资频率直方图
     plt.hist(df['月工资'])
-    # plt.xlabel('工资 (千元)')
-    plt.xlabel('salary(thousand yuan)')
-    # plt.ylabel(u'频数')
-    plt.ylabel('frequency')
-    # plt.title(u"工资直方图")
-    plt.title("salary histogram")
-    plt.savefig('薪资.png')
+    plt.xlabel('工资 (千元)')
+    # plt.xlabel('salary(thousand yuan)')
+    plt.ylabel(u'频数')
+    # plt.ylabel('frequency')
+    plt.title(u"工资直方图")
+    # plt.title("salary histogram")
+    plt.savefig(city+'_'+kind+'_'+'薪资.png')
     plt.show()
 
     #绘制公司分布饼状图
     count = df['区域'].value_counts()
-    plt.pie(count, labels=count.keys(), labeldistance=1.4, autopct='%2.1f%%')
+    plt.pie(count, labels=count.keys(), labeldistance=1.2, autopct='%2.1f%%')
     plt.axis('equal')  # 使饼图为正圆形
     plt.legend(loc='upper left', bbox_to_anchor=(-0.1, 1))
-    plt.savefig('公司分布.png')
+    plt.savefig(city+'_'+kind+'_'+'公司分布.png')
     plt.show()
 
     #绘制学历要求直方图
@@ -211,7 +218,7 @@ if __name__ == '__main__':
         num.append(dict[i])
     print('num:',num)
     plt.bar(index, num, width=0.5)
-    plt.savefig('学历要求.png')
+    plt.savefig(city+'_'+kind+'_'+'学历要求.png')
     plt.show()
 
 
@@ -235,7 +242,7 @@ if __name__ == '__main__':
     cloud.generate_from_text(cut_text)
 
     # 保存词云图片
-    cloud.to_file('word_cloud.jpg')
+    cloud.to_file(city+'_'+kind+'_'+'word_cloud.jpg')
     plt.imshow(cloud)
     plt.axis('off')
     plt.show()
